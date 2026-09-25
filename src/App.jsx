9b3b574f735d9423e015801cb2e65505b011cac4
Ajit Musale <153640394+ajitmusale3505@users.noreply.github.com>
2026-09-25 23:58:56 +0530

    feat: add auth feedback notifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,11 @@
-import AppRoutes from "./routes/AppRoutes";export default function App(){return <AppRoutes/>;}
+import { Toaster } from "react-hot-toast";
+import AppRoutes from "./routes/AppRoutes";
+
+export default function App() {
+  return (
+    <>
+      <AppRoutes />
+      <Toaster position="top-right" toastOptions={{ duration: 3500 }} />
+    </>
+  );
+}
